ab922d069d462208854cca2af57a41a867bdfbc4
pseudowasabi <pseudowasabi@kakao.com>
2025-09-24 20:23:09 +0900

    Examples of styling (inline, css)

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,14 @@ import './App.css'
 
 const Card = ({ title }) => {
     return (
-        <div>
-            <h2>Card Component</h2>
+        <div className="card" style={{
+            border: '1px solid #4b5362',
+            padding: '20px',
+            margin: '10px',
+            backgroundColor: '#31363f',
+            borderRadius: '10px',
+            minHeight: '100px',
+        }}>
             <h2>{title}</h2>
         </div>
     )
